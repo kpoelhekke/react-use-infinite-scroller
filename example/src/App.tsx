9d117c2c2ead44bc0a0dbf9810c59fd49b9cc245
Koen Poelhekke <koen@hubper.co>
2020-06-04 21:39:43 +0200

    Setup basic implementation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from 'react';
+import useInfiniteScroll from 'react-use-infinite-scroller';
+import styled from 'styled-components/macro';
 
-import { ExampleComponent } from 'react-use-infinite-scroller'
-import 'react-use-infinite-scroller/dist/index.css'
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 1em;
+`;
+
+const Item = styled.div`
+  height: 20em;
+  background-color: khaki;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PAGE_LIMIT = 30;
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [limit, setLimit] = React.useState(PAGE_LIMIT);
 
-export default App
+  async function handleLoadMore() {
+    console.log('loading more');
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+
+    setLimit(limit + PAGE_LIMIT);
+  }
+
+  const { containerRef } = useInfiniteScroll({
+    onLoadMore: handleLoadMore,
+  });
+
+  const items = Array(300).fill(null).slice(0, limit);
+
+  return (
+    <Container ref={containerRef}>
+      {items.map((_, index) => (
+        <Item key={index}>{index}</Item>
+      ))}
+    </Container>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,50 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import * as React from 'react';
 
-interface Props {
-  text: string
+export type InfiniteScrollProps = {
+  onLoadMore: () => void;
+  threshold?: number;
+};
+
+const EVENT_TYPES = ['scroll', 'resize'];
+
+function useInfiniteScroll<E extends HTMLDivElement>({
+  onLoadMore,
+  threshold = 250,
+}: InfiniteScrollProps) {
+  const isLoading = React.useRef(false);
+  const containerRef = React.useRef<E>(null!);
+
+  React.useEffect(() => {
+    async function handleScroll() {
+      if (!isLoading.current) {
+        const doc =
+          document.documentElement || document.body.parentNode || document.body;
+        const scrollTop =
+          window.pageYOffset !== undefined ? window.pageYOffset : doc.scrollTop;
+        const { height: documentHeight } = doc.getBoundingClientRect();
+        const delta = documentHeight - scrollTop - window.innerHeight;
+
+        if (delta <= threshold) {
+          isLoading.current = true;
+          await onLoadMore();
+          isLoading.current = false;
+        }
+      }
+    }
+
+    EVENT_TYPES.forEach((event) =>
+      window.addEventListener(event, handleScroll)
+    );
+
+    return () =>
+      EVENT_TYPES.forEach((event) =>
+        window.removeEventListener(event, handleScroll)
+      );
+  }, [onLoadMore, threshold]);
+
+  return {
+    containerRef,
+  };
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export default useInfiniteScroll;
