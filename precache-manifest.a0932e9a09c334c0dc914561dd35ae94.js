6a39c68cb9a51895e68b3df6a27837402eb428c6
Koen Poelhekke <koen@hubper.co>
2020-06-04 21:44:05 +0200

    Updates

---
+++ precache-manifest.a0932e9a09c334c0dc914561dd35ae94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3380afd1fb6733bf9be561e2fa4e9b76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd22bab13456c4e5d9ec",
+    "url": "./static/js/2.c580b428.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c580b428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b438fccca20d5d3c759d",
+    "url": "./static/js/main.da8e6fa7.chunk.js"
+  },
+  {
+    "revision": "c4fd74e0207aea955b62",
+    "url": "./static/js/runtime-main.fd594a1d.js"
+  }
+]);
